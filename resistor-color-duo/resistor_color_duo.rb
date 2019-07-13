62539fe4fad5ece5aec0219391f7d57f59f926cd
TJ Stankus <tjstankus@gmail.com>
2019-07-13 16:52:52 -0400

    Implement resistor-color-duo

---
+++ resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,19 @@
+class ResistorColorDuo
+  
+  RESISTANCES = {
+    "black" => "0",
+    "brown" => "1",
+    "red" => "2",
+    "orange" => "3",
+    "yellow" => "4",
+    "green" => "5",
+    "blue" => "6",
+    "violet" => "7",
+    "grey" => "8",
+    "white" => "9"
+  }
+
+  def self.value(color_codes)
+    color_codes.map { |code| RESISTANCES[code] }.join.to_i
+  end
+end
