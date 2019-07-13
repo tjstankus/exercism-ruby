7720acbb21340ea49b52cde605d153a0306c21dd
TJ Stankus <tjstankus@gmail.com>
2019-07-13 17:36:19 -0400

    Implement rest of high scores

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -4,4 +4,20 @@ class HighScores
   def initialize(scores)
     @scores = scores
   end
+
+  def latest
+    scores.last
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top_three
+    personal_top(3)
+  end
+
+  def personal_top(num)
+    scores.max(num).sort.reverse
+  end
 end
