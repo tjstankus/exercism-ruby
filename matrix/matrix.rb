a135ad9a9667ed1f863e3fdde3639a10ae2b1749
TJ Stankus <tjstankus@gmail.com>
2019-07-13 20:52:59 -0400

    Remove verbosity

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -1,31 +1,8 @@
 class Matrix
-  attr_reader :data, :table
+  attr_reader :rows, :columns
 
   def initialize(data)
-    @data = data
-    @table = init_table
-  end
-
-  def rows
-    table
-  end
-
-  def columns
-    table.transpose  
-  end
-
-  private
-
-  def init_table
-    data                       # "1 2\n10 20"
-      .split("\n")             # ["1 2", "10 20"]
-      .map { |row| row.split } # [["1", "2"], ["10", "20"]]
-      .tap do |a|              # transform strings to integers
-        a.each_with_index do |row, i|
-          row.each_with_index do |_, j|
-            a[i][j] =a[i][j].to_i
-          end
-        end
-      end
+    @rows = data.split("\n").map { |s| s.split.map(&:to_i) }
+    @columns = @rows.transpose
   end
 end
